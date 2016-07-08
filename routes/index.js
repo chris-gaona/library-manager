28f7d7e8a89d18905a507156caa4ff22bb0441f6
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-08 11:33:46 -0700

    Gets all initial nav links and views created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('partials/index', { title: 'Express' });
+  res.render('partials/index', { title: 'Library Manager' });
 });
+
+router.get('/all_books', function(req, res, next) {
+  res.render('partials/all_books', { title: 'Books' });
+});
+
+router.get('/all_patrons', function(req, res, next) {
+  res.render('partials/all_patrons', { title: 'Patrons' });
+});
+
+router.get('/all_loans', function(req, res, next) {
+  res.render('partials/all_loans', { title: 'Loans' });
+});
+
 
 module.exports = router;
