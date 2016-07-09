76996a5bea2194d61b395620566facdc9cda993b
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-09 10:33:50 -0700

    Adds method-override npm package; adds functionality to edit existing book

--- routes/book.js
+++ routes/book.js
@@ -7,7 +7,7 @@ var books = require('../models').books;
 var loans = require('../models').loans;
 var patrons = require('../models').patrons;
 
-// GET BOOKS
+/* GET all books & filter by overdue & checked out. */
 router.get('/books', function(req, res, next) {
   if (req.query.filter === 'overdue') {
     loans.findAll({ include: [{ model: books }], where: { return_by: { $lt: new Date() }, returned_on: null }
@@ -46,12 +46,12 @@ router.get('/books', function(req, res, next) {
   }
 });
 
-// FORM
+/* Create new book form. */
 router.get('/books/new', function(req, res, next) {
   res.render('partials/new_book', { title: 'New Book' });
 });
 
-// CREATE NEW BOOK
+/* POST add new book. */
 router.post('/books/new', function (req, res, next) {
   books.create(req.body).then(function (book) {
     res.redirect('/books');
@@ -73,6 +73,7 @@ router.post('/books/new', function (req, res, next) {
   });
 });
 
+/* GET inidividual book. */
 router.get('/books/:id', function(req, res, next) {
   books.findAll({ include: [{ model: loans, include: [{ model: patrons }] }], where: { id: req.params.id }}).then(function (book) {
     if (book) {
@@ -87,12 +88,66 @@ router.get('/books/:id', function(req, res, next) {
         first_published: getLoans[0].first_published
       };
       for (var i = 0; i < getLoans.length; i++) {
-        loanArray.push(getLoans[i].loan);
+        if (getLoans[i].loan === null) {
+          loanArray = [];
+        } else {
+          loanArray.push(getLoans[i].loan);
+        }
       }
 
       res.render('partials/book_details', { book: bookObject, loans: loanArray, title: book.title });
     } else {
       res.sendStatus(404);
+    }
+  }).catch(function (err) {
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
+
+/* PUT update book. */
+router.put('/books/:id', function (req, res, next) {
+  books.findById(req.params.id).then(function (book) {
+    if (book) {
+      return book.update(req.body);
+    } else {
+      res.sendStatus(404);
+    }
+  }).then(function (book) {
+    res.redirect("/books/" + book.id);
+  }).catch(function (err) {
+    if (err.name === "SequelizeValidationError") {
+      books.findAll({ include: [{ model: loans, include: [{ model: patrons }] }], where: { id: req.params.id }}).then(function (book) {
+        if (book) {
+          var bookObject = {};
+          var loanArray = [];
+          var getLoans = JSON.parse(JSON.stringify(book));
+          bookObject = {
+            id: getLoans[0].id,
+            title: getLoans[0].title,
+            author: getLoans[0].author,
+            genre: getLoans[0].genre,
+            first_published: getLoans[0].first_published
+          };
+          for (var i = 0; i < getLoans.length; i++) {
+            if (getLoans[i].loan === null) {
+              loanArray = [];
+            } else {
+              loanArray.push(getLoans[i].loan);
+            }
+          }
+
+          res.render('partials/book_details', { book: bookObject, loans: loanArray, title: book.title, errors: err.errors });
+        } else {
+          res.sendStatus(404);
+        }
+      }).catch(function (err) {
+        console.log(err);
+        res.sendStatus(500);
+      });
+    } else {
+      // throw error to be handled by final catch
+      throw err;
     }
   }).catch(function (err) {
     console.log(err);
