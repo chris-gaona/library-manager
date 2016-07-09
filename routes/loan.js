a0db6ba01c337caa33ac0e0245f75f18e9880df0
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-09 12:30:44 -0700

    Adds required data to new loan page from database

--- routes/loan.js
+++ routes/loan.js
@@ -7,7 +7,7 @@ var books = require('../models').books;
 var loans = require('../models').loans;
 var patrons = require('../models').patrons;
 
-router.get('/loans', function(req, res, next) {
+router.get('/loans', function (req, res, next) {
   if (req.query.filter === 'overdue') {
     loans.findAll({ include: [{ model: books }, { model: patrons }], where: { return_by: { $lt: new Date() }, returned_on: null }
     }).then(function (overdueBooks) {
@@ -33,6 +33,30 @@ router.get('/loans', function(req, res, next) {
       res.sendStatus(500);
     });
   }
+});
+
+router.get('/loans/new', function (req, res, next) {
+  var today = new Date();
+  var addAWeek = new Date();
+  addAWeek.setDate(today.getDate() + 7);
+
+  books.findAll({
+    attributes: ['id', 'title'],
+    order: 'title'
+}).then(function (books) {
+    patrons.findAll({
+      attributes: ['id', 'first_name', 'last_name'],
+      order: 'last_name'
+    }).then(function (patrons) {
+      res.render('partials/new_loan', { books: books, patrons: patrons, today: today, due: addAWeek, title: 'New Loan' });
+    }).catch(function (err) {
+      console.log(err);
+      res.sendStatus(500);
+    });
+  }).catch(function (err) {
+    console.log(err);
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
