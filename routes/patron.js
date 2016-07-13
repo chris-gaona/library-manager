20e2f9f35cf7b8a5069541d2f59a4b469269ec1e
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-13 14:34:26 -0700

    Fixes some issues with patron details page links

--- routes/patron.js
+++ routes/patron.js
@@ -24,9 +24,10 @@ router.get('/patrons/page/:page', function(req, res, next) {
 router.get('/patrons/:id', function(req, res, next) {
   patrons.findById(req.params.id, {
   }).then(function (patron) {
-    loans.findAll({ include: [{ model: books, attributes: ["title"] }, { model: patrons, where: { id: req.params.id }, attributes: ["first_name", "last_name"] }]
+    loans.findAll({ include: [{ model: books, attributes: ["id", "title"] }, { model: patrons, where: { id: req.params.id }, attributes: ["first_name", "last_name"] }]
     }).then(function (results) {
       if (results) {
+        console.log(JSON.stringify(results));
         res.render('partials/patron_details', { patron: patron, results: results, title: patron.first_name + ' ' + patron.last_name });
       }
     }).catch(function (err) {
